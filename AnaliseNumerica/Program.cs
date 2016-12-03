bad7de4e9d2435fd35a4d25a9c94a61ae9107b62
unknown <leonardo.giroto@gmail.com>
2016-12-03 18:59:18 -0200

    Leitura do Arquivo RAW & Criação de Exemplo PMG & Inclusão Biblioteca de Cálculo de Integral (para teste)

--- AnaliseNumerica/Program.cs
+++ AnaliseNumerica/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using MathNet.Numerics.Integration;
 
 namespace AnaliseNumerica
 {
@@ -10,7 +8,71 @@ namespace AnaliseNumerica
     {
         static void Main(string[] args)
         {
+            byte[] ImageContent = ReadRawFile();
 
+            DefinePixels(ImageContent);
+
+            WritePGMFile();
         }
+
+        private static byte[] ReadRawFile()
+        {
+            try
+            {
+                byte[] data = File.ReadAllBytes(@"..\..\Assets\head-8bit.raw");
+                return data;
+            }
+            catch (Exception Error)
+            {
+                Console.WriteLine(Error);
+                throw;
+            }
+        }
+
+        private static void DefinePixels(byte [] ImageContent)
+        {
+            //double Valor = SimpsonRule.IntegrateComposite(x => x * x, 0.0, 10.0, 4);
+            //Console.WriteLine(Valor);
+        }
+
+        private static void WritePGMFile()
+        {
+            string currentLine = "P2";
+
+            try
+            {
+                StreamWriter file = new StreamWriter(@"..\..\Results\Resultado.pgm");
+                file.WriteLine(currentLine);
+
+                currentLine = "# Descrição da imagem";
+                file.WriteLine(currentLine);
+
+                currentLine = "5 5"; // Dimensão dela, largura x altura
+                file.WriteLine(currentLine);
+
+                currentLine = "255"; // Maior valor encontrado de pixel na imagem
+                file.WriteLine(currentLine);
+
+                // Exemplo
+                currentLine = "0 255 255 255 255";
+                file.WriteLine(currentLine);
+                currentLine = "255 0 255 255 255";
+                file.WriteLine(currentLine);
+                currentLine = "255 255 0 255 255";
+                file.WriteLine(currentLine);
+                currentLine = "255 255 255 0 255";
+                file.WriteLine(currentLine);
+                currentLine = "255 255 255 255 0";
+                file.WriteLine(currentLine);
+
+                file.Close();
+            }
+            catch (Exception Error)
+            {
+                Console.WriteLine(Error);
+                throw;
+            }
+        }
+
     }
 }
