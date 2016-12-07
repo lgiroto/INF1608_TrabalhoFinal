8780b57107f783c13e848fdc0740d4c38c465a12
Mauricio Pedro Vieira <mauriciopsgv@gmail.com>
2016-12-07 18:11:47 -0200

    Esqueleto da classe do conteudo da Imagem

---
+++ AnaliseNumerica/ImageContent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnaliseNumerica
+{
+    class ImageContent
+    {
+        public byte[] conteudo;
+        
+        public double DensityValue (int position ,int layer, double value)
+        {
+            return 3;
+        }
+
+        public double OpacityValue (int position, int layer, double value)
+        {
+            return 4;
+        }
+    }
+}
