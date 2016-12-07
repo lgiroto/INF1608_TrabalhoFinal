53c2ec601ce8e4d5813e11a4aa4c8e59a560fd97
Mauricio Pedro Vieira <mauriciopsgv@gmail.com>
2016-12-07 19:04:56 -0200

    Metodos da classe image content implementados

--- AnaliseNumerica/ImageContent.cs
+++ AnaliseNumerica/ImageContent.cs
@@ -4,20 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace AnaliseNumerica
 {
     class ImageContent
     {
+        public const int sizeX = 256;
+        public const int sizeY = 256;
+        public const int resolutionX = 128;
+        public const int resolutionZ = 99;
+        public const int numLayer = 99;
         public byte[] conteudo;
         
         public double DensityValue (int position ,int layer, double value)
         {
-            return 3;
+            //Implementacao ainda sem a interpolacao linear
+            int index = Convert.ToInt32(layer * sizeX * sizeY + value * sizeX + position);
+            return conteudo[index];
         }
 
         public double OpacityValue (int position, int layer, double value)
         {
-            return 4;
+            if (DensityValue(position,layer,value) < 0.3)
+            {
+                return 0;
+            }
+            // Nessa parte eu nao tenho certeza se eh a densidade mesmo que ele quer
+            // No trabalho ele chama de "v" mas esse "v" nao aparece em nenhum lugar
+            return 0.05 * (DensityValue(position, layer, value) - 0.3);
         }
     }
 }
